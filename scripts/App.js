a487707c564f25d803313a443c56b0dbd9ba6759
Michal Bonowicz <bonowiczmichal1332@gmail.com>
2022-05-15 22:48:34 +0200

    while scrolling, the player appears in the corner

--- scripts/App.js
+++ scripts/App.js
@@ -1,4 +1,6 @@
 const playerVideo = document.querySelector(".video-js")
+const playerContainer = document.querySelector(".player__container")
+let playerContainerHeight = null
 
 const options = {
     controls: true,
@@ -6,8 +8,20 @@ const options = {
     preload: 'auto',
     fluid: true
 }
+
 const player = videojs(playerVideo, options)
 
 player.ready(function () {
-    console.log(this)
+    window.addEventListener('scroll', () => {
+        playerContainerHeight = playerContainer.clientHeight
+        if (window.scrollY > playerContainerHeight) {
+            playerContainer.classList.add("player__container--fixed")
+            playerContainer.classList.remove("player__container")
+
+        } else {
+            playerContainer.classList.add("player__container")
+            playerContainer.classList.remove("player__container--fixed")
+        }
+    })
 })
+
